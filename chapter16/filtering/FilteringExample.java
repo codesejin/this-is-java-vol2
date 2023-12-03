4e28f18bf304548c7bef3a6da8d54239d1794656
sejin park <seijin0722@naver.com>
2023-12-04 02:32:41 +0900

    :bulb: 필터링, 매핑, 정렬, 루핑

---
+++ chapter16/filtering/FilteringExample.java
@@ -0,0 +1,27 @@
+package org.example.chapter16.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FilteringExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("홍길동","신용권","김자바","신용권","신민철");
+
+        names.stream()
+                .distinct() // 중복 제거
+                .forEach(n -> System.out.println(n));
+
+        System.out.println();
+
+        names.stream()
+                .filter(n -> n.startsWith("신")) // 필터링
+                .forEach(n -> System.out.println(n));
+
+        System.out.println();
+
+        names.stream()
+                .distinct() // 중복 제거 후 필터링
+                .filter(n -> n.startsWith("신"))
+                .forEach(n -> System.out.println(n));
+    }
+}
