0cc4d2abb03f1ce530440bd2ec0dda168f02497d
sejin park <seijin0722@naver.com>
2023-12-23 23:36:42 +0900

    :bulb: 스레드 제어

--- chapter14/control/SleepExample.java
+++ chapter14/control/SleepExample.java
@@ -1,2 +1,19 @@
-package org.example.chapter14.control;public class SleepExample {
+package org.example.chapter14.control;
+
+import java.awt.*;
+
+// 3초 주기로 10번 비프음 발생
+public class SleepExample {
+
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for (int i = 0; i < 10; i++) {
+            toolkit.beep();
+            try {
+                Thread.sleep(3000); // 3초 동안 메인 스레드를 일시 정지 상태를 만듬
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
 }
--- chapter14/control/YieldExample.java
+++ chapter14/control/YieldExample.java
@@ -1,4 +1,22 @@
 package org.example.chapter14.control;
 
+//스레드 실행 양보 예제
 public class YieldExample {
+    public static void main(String[] args) {
+        ThreadA threadA = new ThreadA();
+        ThreadB threadB = new ThreadB();
+        // ThreadA, ThreadB 모두 실행
+        threadA.start();
+        threadB.start();
+
+        try { Thread.sleep(3000); } catch (InterruptedException e) {}
+        threadA.work = false;// ThreadB만 실행
+
+        try { Thread.sleep(3000); } catch (InterruptedException e) {}
+        threadA.work = true; // ThreadA, ThreadB 모두 실행
+
+        try { Thread.sleep(3000); } catch (InterruptedException e) {}
+        threadA.stop = true; // ThreadA, ThreadB 모두 종료
+        threadB.stop = true;
+    }
 }
